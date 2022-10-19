3cd3e366dc7aebd4190b8a401a8354724177deca
orucmu <mustafaoruc720@gmail.com>
2022-10-19 14:12:18 +0300

    first commit

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+
+function Router(){
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  )
+}
+
+export default Router;
